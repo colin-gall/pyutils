3c8f24201de642816548fb2c2bcdd964abe87655
Colin Gallagher <39956444+colin-gall@users.noreply.github.com>
2020-02-28 16:34:23 -0500

    Update __init__.py

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -3,3 +3,6 @@ from pyutils.shell import clear_screen
 from pyutils.shell import flush_screen
 from pyutils.shell import progress_bar
 from pyutils.shell import current_time
+
+from pyutils.web import scrape_url
+from pyutils.web import check_connection
