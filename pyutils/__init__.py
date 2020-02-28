f6cfcde9e238f6115182aa1b6a7b96dcca7fa7f4
Colin Gallagher <39956444+colin-gall@users.noreply.github.com>
2020-02-28 16:29:33 -0500

    Update __init__.py

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -1,1 +1,5 @@
-
+from pyutils.shell import colors
+from pyutils.shell import clear_screen
+from pyutils.shell import flush_screen
+from pyutils.shell import progress_bar
+from pyutils.shell import current_time
