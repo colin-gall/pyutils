ca4589e53aeb7caeccbe0b95c620e467df0f1fdd
Colin Gallagher <39956444+colin-gall@users.noreply.github.com>
2020-02-28 16:19:11 -0500

    Create shell.py

---
+++ pyutils/shell.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class colors:
+	reset='\033[0m'
+	bold='\033[01m'
+	underline='\033[04m'
+	red='\033[91m'
+	green='\033[92m'
+	yellow='\033[93m'
+	pink='\033[95m'
+	cyan='\033[96m'
+	grey='\033[90m'
+	white='\033[97m'
